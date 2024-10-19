44584ca41c0dc53ac17179d051f9a58fd4ac32a7
mikayilov2006@gmail.com <mikayilov2006@gmail.com>
2024-10-19 19:11:59 +0400

    App.SCSS  add and app.jsx

--- Search/src/App.jsx
+++ Search/src/App.jsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import "./App.scss"
+const App = () => {
 
-function App() {
-  const [count, setCount] = useState(0)
+  const students = [
+    {
+      name: 'Yasemen',
+      surname: 'Memmedova',
+      age: 18,
+      isWorking: true,
+      hasVisa: false,
+    },
+    {
+      name: 'Jabbar',
+      surname: 'Jabbarli',
+      age: 22,
+      isWorking: false,
+      hasVisa: true,
+    },
+    {
+      name: 'Fatime',
+      surname: 'Ehmedova',
+      age: 20,
+      isWorking: true,
+      hasVisa: true,
+    },
+    {
+      name: 'Fuad',
+      surname: 'Mikayilzade',
+      age: 24,
+      isWorking: false,
+      hasVisa: false,
+    },
+    {
+      name: 'Orxan',
+      surname: 'Eliyev',
+      age: 27,
+      isWorking: false,
+      hasVisa: true,
+    },
+    {
+      name: 'Elxan',
+      surname: 'Meherremli',
+      age: 32,
+      isWorking: true,
+      hasVisa: true,
+    },
+  ];
+
+  const [search, setSearch] = useState('')
+  
+  const inptChange = (e) => {
+    setSearch(e.target.value)
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="page">
 
-export default App
+      
+      <input
+        onChange={inptChange}
+        type="text"
+        placeholder="Find..."
+      />
+
+
+      <section className="demo">
+        <div className="calculator-wrapper">
+          {students.filter(item=> item.name.toUpperCase().includes(search.toUpperCase())).map((item, index) => {
+            return (
+              <div className="workerCard">
+                <h2>
+                  FulName: {item.name} {item.surname}
+                </h2>
+                <h2>Is working: {item.isWorking} </h2>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default App;
